0f856c7f84f13970980d45bc4129f336ee0bfc4b
Michael <michaelbenjamin12.mb@outlook.com>
2021-07-22 00:50:40 +0200

    Created repository package and created main generic repository interface

--- src/main/java/za/ac/cput/repository/IRepository.java
+++ src/main/java/za/ac/cput/repository/IRepository.java
@@ -1,12 +1,12 @@
 package za.ac.cput.repository;
 
-public interface IRepository <T>{
+public interface IRepository <T, ID>{
     T create(T t);
 
-    T read();
+    T read(ID id);
 
     T update(T t);
 
-    void delete();
+    void delete(ID id);
 
 }
